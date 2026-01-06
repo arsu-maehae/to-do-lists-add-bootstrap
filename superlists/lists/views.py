62c902595873919af23dfa263c51246235698f07
arsumaehae <arsumaehae@gmail.com>
2026-01-06 11:13:17 +0700

    comit superlist

---
+++ superlists/lists/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from lists.models import Item
+from django.shortcuts import render, redirect  # <--- เพิ่ม redirect
+
+def home_page(request):
+    if request.method == "POST":
+        Item.objects.create(text=request.POST["item_text"])
+        return redirect("/")
+
+    # ดึงข้อมูลทั้งหมดออกมา
+    items = Item.objects.all()
+    # ส่งไปที่ template ในชื่อ 'items'
+    return render(request, "home.html", {"items": items})
---
+++ superlists/test_selenium.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+# Selenium Manager automatically finds and downloads the driver for you
+driver = webdriver.Chrome()
+
+driver.get("https://docs.djangoproject.com/en/6.0/intro/tutorial01/")
+print(driver.title)
+assert("part 1" in driver.title)
+driver.quit()
